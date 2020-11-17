72585283084fe2ab63d397b9eb06874a8af9468a
igor <igor.kryvoruchko.88@gmail.com>
2020-11-17 22:26:02 +0200

    my first vue component

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{any}', 'App\Http\Controllers\PagesController@index')->where('any', '.*');
